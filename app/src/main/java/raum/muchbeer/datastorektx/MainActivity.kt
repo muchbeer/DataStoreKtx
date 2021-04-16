68d3f62262cfa94e987330a290dc1be634e8f57c
muchbeer <gman2them@gmail.com>
2021-04-16 16:18:39 +0300

    using DataBinding and viewModel in SwitchCompat refining adding retrieving random messages adding hide keyboard

--- app/src/main/java/raum/muchbeer/datastorektx/MainActivity.kt
+++ app/src/main/java/raum/muchbeer/datastorektx/MainActivity.kt
@@ -19,9 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-      //  binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //This is for viewBinding
+        /*binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)*/
         userPref = UserPref(this)
         viewModelFactory = UserPrefViewModelFactory(userPref)
         viewModel = ViewModelProvider(this, viewModelFactory).get(UserPrefViewModel::class.java)
